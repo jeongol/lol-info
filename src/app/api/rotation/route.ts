5ad71eb3b8134fe0bd6fd3ed8464d5f8a1fd22fd
jeongol <jungcode127@gmail.com>
2024-10-02 15:21:59 +0900

    fix: 로테이션 챔피언 api 엔드포인트 코드 수정

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,40 +1,22 @@
-// app/api/rotation/route.ts
 import { NextResponse } from "next/server";
 import { ChampionRotation } from "../../../types/ChampionRotation.ts";
 
-export async function GET() {
-  const apiKey = process.env.RIOT_API_KEY;
+// api key 가져오기
+const apiKey = process.env.RIOT_API_KEY;
 
-  if (!apiKey) {
-    return NextResponse.json({ error: "API key 없음" }, { status: 500 });
-  }
-
-  const apiUrl =
-    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
-
-  try {
-    // Riot Games API 호출
-    const response = await fetch(apiUrl, {
+// riot api 호출
+export async function GET(request: Request) {
+  const res = await fetch(
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+    {
       headers: {
-        "X-Riot-Token": apiKey,
+        "X-Riot-Token": `${apiKey}`,
       },
-    });
-
-    if (!response.ok) {
-      return NextResponse.json(
-        { error: "챔피언 로테이션 불러오기 실패" },
-        { status: 500 }
-      );
     }
+  );
 
-    // 응답 데이터를 json 변환
-    const data: ChampionRotation = await response.json();
-
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
+  // ChampionRotation 타입으로 변환
+  const data: ChampionRotation = await res.json();
+  // json 형태로 응답
+  return NextResponse.json({ data });
 }
